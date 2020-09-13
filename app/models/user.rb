b283b577c1f4a27e64e3db20773e3ba631fad8cd
nishi622 <nishiiiiiii622@gmil.com>
2020-09-13 23:47:11 +0900

    バリデーション編集とRspecの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   with_options presence: true do       
-    validates :email, uniqueness: true
-    validates :email, inclusion: {in: /[@]/}
-    validates :password, format: {with: /\A(?=.*?a-z)(?=.*?[\d])[a-z\d]+\z/i}
-    validates :password, length:{minimum: 6}
-    with_options presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/} do
+    validates :email, uniqueness: true, inclusion: {in: /[@]/} 
+    validates :password, format: {with: /\A(?=.*?a-z)(?=.*?[\d])[a-z\d]+\z/i}, length: {minimum: 6}
+    with_options format: { with: /\A[ぁ-んァ-ンー-龥]+\z/} do
       validates :last_name_kanzi
       validates :first_name_kanzi
     end
-    with_options presence: true, format: { with: /\A[ァ-ン]+\z/} do
+    with_options format: { with: /\A[ァ-ン]+\z/} do
       validates :last_name_kana
       validates :first_name_kana
     end
