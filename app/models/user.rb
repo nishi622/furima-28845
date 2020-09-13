bfdae049ba2affa1f6d7d4208740aa82f1b6c4f8
nishi622 <nishiiiiiii622@gmil.com>
2020-09-13 18:41:12 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do       
+    validates :email, uniqueness: true
+    validates :email, inclusion: {in: /[@]/}
+    validates :password, format: {with: /\A(?=.*?a-z)(?=.*?[\d])[a-z\d]+\z/i}
+    validates :password, length:{minimum: 6}
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/} do
+      validates :last_name_kanzi
+      validates :first_name_kanzi
+    end
+    with_options presence: true, format: { with: /\A[ァ-ン]+\z/} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    validates :birthday
+  end
 end
