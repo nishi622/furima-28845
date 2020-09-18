5b18acb5a7e5fc0ae3ca29bd9faa44975618105a
nishi622 <nishiiiiiii622@gmil.com>
2020-09-18 12:48:32 +0900

    rubocopの導入と実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  
-  validates :name, presence: true 
+
+  validates :name, presence: true
   validates :email, uniqueness: true
-  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-  with_options presence: true do  
-    validates :birthday     
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  with_options presence: true do
+    validates :birthday
     # validates :password, format: {with: /\A(?=.*?a-z)(?=.*?[\d])[a-z\d]+\z/i}, length: {minimum: 6}
-    with_options format: { with: /\A[ぁ-んァ-ンー-龥]+\z/} do
+    with_options format: { with: /\A[ぁ-んァ-ンー-龥]+\z/ } do
       validates :last_name_kanzi
       validates :first_name_kanzi
     end
-    with_options format: { with: /\A[ァ-ン]+\z/} do
+    with_options format: { with: /\A[ァ-ン]+\z/ } do
       validates :last_name_kana
       validates :first_name_kana
     end
-    
   end
 end
