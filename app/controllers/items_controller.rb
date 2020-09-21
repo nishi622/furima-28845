e92054d53170e1c47b20abb9f1099d71417aa8d8
nishi622 <nishiiiiiii622@gmil.com>
2020-09-22 01:57:00 +0900

    ログインしていないユーザーを出品画面に遷移させない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+  
   def index
   end
 
@@ -15,9 +17,17 @@ class ItemsController < ApplicationController
     end
   end
 
+  
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :delivery_fee_id, :delivery_area_id, :delivery_day_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
