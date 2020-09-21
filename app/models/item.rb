99b81c8d7dfe99914f2d7d0631ee8ee8692e3c5a
nishi622 <nishiiiiiii622@gmil.com>
2020-09-21 10:28:59 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :status, :delivery_fee, :delivery_area, :delivery_day
 end
