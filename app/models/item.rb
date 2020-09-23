3a81ccdfcd8c79d8481e775fb61e11f0be766caf
nishi622 <nishiiiiiii622@gmil.com>
2020-09-23 10:54:38 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,9 @@ class Item < ApplicationRecord
   belongs_to_active_hash :delivery_area
   belongs_to_active_hash :delivery_day
   has_one_attached :image
+
+  validates :name, :explanation, :category, :status, :delivery_fee, :delivery_area, :delivery_day, :price, presence: true
+  validates :category_id, :status_id, :delivery_fee_id, :delivery_area_id, :delivery_day_id, numericality: { other_than: 1 } 
+  validates :price, numericalty: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  validates :price, format: {with: /\A[0-9]+\z/}
 end
